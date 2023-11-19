948fc241936e7259a99cf780e7ebc885fefe66a6
Alex19951209 <ovcharuk19951209@gmail.com>
2023-11-19 21:57:13 +0100

    We add the administrator interface

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -8,3 +8,16 @@ class Topic(models.Model):
 	def __str__(self):
 		"""Повернути рядкове представлення моделі."""
 		return self.text
+
+class Entry(models.Model):
+	"""Якась конкретна інформація до цієї теми."""
+	topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		verbose_name_plural = 'entries'
+
+	def __str__(self):
+		"""Провертає прежставлення моделі у string."""
+		return f"{self.text[:50]}..."
