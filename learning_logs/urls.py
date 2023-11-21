53d5ce7a80b8ed2cc76b5d90e52b5fc8294e76b6
Alex19951209 <ovcharuk19951209@gmail.com>
2023-11-21 23:16:02 +0100

    Creation of the main page

---
+++ learning_logs/urls.py
@@ -0,0 +1,11 @@
+"""Defines URL patterns for learning_logs."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Головна сторінка.
+    path('', views.index, name='index'),
+]
