1961963b63429df17c38cd6f3707949c49f4c1ff
Alex19951209 <ovcharuk19951209@gmail.com>
2023-11-22 23:20:48 +0100

    Page with topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Головна сторінка.
     path('', views.index, name='index'),
+    # Сторінка, що відображає всі теми.
+    path('topics/', views.topics, name='topics'),
+    # Сторінка, присвячена окремій темі.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
