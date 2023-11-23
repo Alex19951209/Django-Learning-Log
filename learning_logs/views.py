47627d89f5c94ffe29ae8f3e0ab8e1c035d9fea3
Alex19951209 <ovcharuk19951209@gmail.com>
2023-11-23 23:10:06 +0100

    Editing posts

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 def index(request):
 	"""Головна сторінка "Журналу спостережень"."""
@@ -35,3 +35,22 @@ def new_topic(request):
 	# Показати порожню або недійсну форму.
 	context = {'form': form}
 	return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+	"""Add a new entry for a particular topic."""
+	topic = Topic.objects.get(id=topic_id)
+	if request.method != 'POST':
+		# Жодних даних не надіслано; створити порожню форму.
+		form = EntryForm()
+	else:
+		# Отримані дані у POST - запиті; обробити дані.
+		form = EntryForm(data=request.POST)
+		if form.is_valid():
+			new_entry = form.save(commit=False)
+			new_entry.topic = topic
+			new_entry.save()
+			return redirect('learning_logs:topic', topic_id=topic_id)
+
+	# Показати порожню або недійсну форму.
+	context = {'topic': topic, 'form': form}
+	return render(request, 'learning_logs/new_entry.html', context)
