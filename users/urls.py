4f0cf25bc731eff7abab1e33a366dee83ace21f5
Alex19951209 <ovcharuk19951209@gmail.com>
2023-12-05 22:52:19 +0100

    Add register's page.

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,13 @@
 
 from django.urls import path, include
 
+from .import views
+
 app_name = 'users'
 
 urlpatterns = [
 	# Додати уставні URL auth (автентифікації).
 	path('', include('django.contrib.auth.urls')),
+	# торінка реєстрації.
+	path('register/', views.register, name='register'),
 ]
